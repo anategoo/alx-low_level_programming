deb6470b86be35314cfc046a6641c865fffea372
adham <adhamalpap27@gmail.com>
2023-10-13 12:39:56 +0000

    My special commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	for (int a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
