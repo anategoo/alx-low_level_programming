3113f7ecbfed35a27db0e19c1b27493e86141ad4
adham <adhamalpap27@gmail.com>
2023-10-27 01:13:04 +0000

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,11 @@
  */
 int _pow_recursion(int x, int y) {
     if (y < 0) {
-        return -1; // If y is negative, return -1 (error condition).
+        return (-1); // If y is negative, return -1 (error condition).
     }
     if (y == 0) {
-        return 1; // Any number raised to the power of 0 is 1.
+        return (1); // Any number raised to the power of 0 is 1.
     }
 
-    return x * _pow_recursion(x, y - 1); // Recursively compute x^y as x * x^(y-1).
+    return (x * _pow_recursion(x, y - 1)); // Recursively compute x^y as x * x^(y-1).
 }
