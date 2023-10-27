7d206a4cc70063fad52a01ae63dfc4e7d36c79a2
adham <adhamalpap27@gmail.com>
2023-10-27 20:28:43 +0000

    0x09-static-libraries

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * _isalpha - check for lower case letter
+ *   * @c : character to check
+ *    * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
