0d737d6dba3bd035b85d5b371397f1345d301cc1
adham <adhamalpap27@gmail.com>
2023-11-16 21:32:52 +0000

    asd

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+size_t print_list(const list_t *h);
+#endif
